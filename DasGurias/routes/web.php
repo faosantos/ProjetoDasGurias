9a7fe9fb96e7ec4bf0dd353d8e4f4396725f9e98
faosantos <faosantos@restinga.ifrs.edu.br>
2019-06-11 01:37:53 -0300

    commit

--- DasGurias/routes/web.php
+++ DasGurias/routes/web.php
@@ -11,8 +11,8 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
 Route::get('/home', 'HomeController@index')->name('home');
